cf2a4fcc5757909c3cbca94fd09cab563041965d
jlhonora <jlhonora@ing.puc.cl>
2015-06-21 23:48:06 -0300

    Fixed SA algorithm

--- optim-simulated-annealing.go
+++ optim-simulated-annealing.go
@@ -6,49 +6,53 @@ import (
 )
 
 const STARTING_TEMP = 500.0
-const COOLING_RATE = 0.003
+const COOLING_RATE = 0.002
 
 func OptimizeRouteSimulatedAnnealing(routeProblem *RouteProblem) {
 	route := routeProblem.Route
 	lastWaypoint := route.Start
+	pointsLength := len(route.Points)
 
 	// Get a starting point
 	_, closestIndex := GetClosest(route.Points, lastWaypoint)
 
+	// The closest point will always be the first one
+	route.Points.Swap(0, closestIndex)
+
 	var proposedRoute Route
 
-	// The closest point will always be the first one
-	route.Points[closestIndex], route.Points[0] = route.Points[0], route.Points[closestIndex]
-	pointsLength := len(route.Points)
+	proposedRoute.Points = make([]Point, pointsLength)
+	clonePoints(pointsLength, *route, &proposedRoute)
+	proposedRoute.Start = route.Start
 
-	proposedRoute.Points = route.Points
-
+	currentDistance := route.TotalDistance()
 	currentTemp := STARTING_TEMP
+	iter := 0
 	for currentTemp > 1 {
-		shuffle(pointsLength, route, &proposedRoute)
-		currentDistance := route.TotalDistance()
+		shuffle(pointsLength, &proposedRoute)
 		proposedDistance := proposedRoute.TotalDistance()
 
+		// Only for debugging/plotting purposes
+		// fmt.Println(iter, currentTemp, currentDistance, proposedDistance)
+
 		if shouldAcceptRoute(currentTemp, currentDistance, proposedDistance) {
-			route.Points = proposedRoute.Points
+			currentDistance = proposedDistance
+			clonePoints(pointsLength, proposedRoute, route)
 		}
 		currentTemp *= (1 - COOLING_RATE)
-	}
 
-	// Assign slots by order
-	for i := 0; i < len(route.Points); i++ {
-		route.Points[i].Slot = i
+		iter += 1
 	}
 }
 
-func shuffle(pointsLength int, route *Route, shuffledRoute *Route) {
-	shuffledRoute.Points = route.Points
+func shuffle(pointsLength int, shuffledRoute *Route) {
 	// The first point never moves
 	index_a := rand.Intn(pointsLength-1) + 1
 	index_b := rand.Intn(pointsLength-1) + 1
 
-	shuffledRoute.Points[index_a], shuffledRoute.Points[index_b] = shuffledRoute.Points[index_b], shuffledRoute.Points[index_a]
-	shuffledRoute.Points[index_a].Slot, shuffledRoute.Points[index_b].Slot = shuffledRoute.Points[index_b].Slot, shuffledRoute.Points[index_a].Slot
+	shuffledRoute.Points.Swap(index_a, index_b)
+	shuffledRoute.Points[index_a].Slot = index_a
+	shuffledRoute.Points[index_b].Slot = index_b
 }
 
 func shouldAcceptRoute(temperature float64, currentDistance float64, proposedDistance float64) bool {
